dfb5b03ca6a4eb80fd8467e5a5ba4d900af630a6
ctlee <ctlee@ucsd.edu>
2018-06-14 19:18:01 -0700

    Updating tgzs with updated HMR MD Runfiles + adding bccHelper script

---
+++ docs/assets/pdbs/bccHelper.py
@@ -0,0 +1,19 @@
+
+# List of BioChemCoRe BCC IDs corresponding to structures
+bccids = ['0YDD5', '6WCGO', 'A73SR', 'DJ1H3', 'JHGSB', 'LLCXM', 'NEQSA', 'NIZGV', 'NLSYQ', 'OVHRZ', 'TNRT6', 'VEH1I']
+
+# Dictionary mapping BCCID to ligand residue name
+bccid_to_lig = {
+'TNRT6' : '05S',
+'VEH1I' : '06H',
+'NIZGV' : '2D9',
+'DJ1H3' : '2DD',
+'OVHRZ' : '99A',
+'0YDD5' : '99B',
+'NLSYQ' : 'B2K',
+'NEQSA' : 'B2L',
+'6WCGO' : 'PU1',
+'A73SR' : 'PU2',
+'JHGSB' : 'ZZ2',
+'LLCXM' : 'ZZ3',
+}
