78d3a99b6b0a6c04c273c0eaa54e26ede577c111
Christian Seitz <30423859+cgseitz@users.noreply.github.com>
2018-07-10 09:52:35 -0700

    cleaned up comments

--- docs/assets/pdbs/PCA_tutorial.py
+++ docs/assets/pdbs/PCA_tutorial.py
@@ -12,32 +12,24 @@ from sklearn.decomposition import PCA
 import sys
 
 #load the MD trajectory
-#traj = md.load('$trajectory_name')
-#nc = /net/gpfs/jam-amaro-shared/bcc2018_trajectories/0YDD5-Pro01.nc
-#param = /net/gpfs/jam-amaro-shared/bcc2018_trajectories/0YDD5.prmtop
 traj = md.load_netcdf(filename='/scratch/bcc2018_trajectories/6WCGO/md1/6WCGO-Pro01.nc',top='/scratch/bcc2018_trajectories/6WCGO/6WCGO.prmtop')
 traj
-
-#the trajectory must be entered on the command line after this script
-#trajectory_name=$1
 
 #we want to project our data into 2D, this sets up a 2D (replace n with 2)
 pca1 = PCA(n_components=2)
 traj.superpose(traj, 0)
 
-#for more PCA
+#for n principal components, put the number you want here
 pca_all = PCA(n_components=10)
 
-#reshapes the data into the system created above, edit this for 2 components
+#reshapes the data into the 2 component system created above
 reduced_cartesian = pca1.fit_transform(traj.xyz.reshape(traj.n_frames, traj.n_atoms * 3))
 print(reduced_cartesian.shape)
 
+#reshapes the data for the n principal component system
 non_reduced_cartesian = pca_all.fit_transform(traj.xyz.reshape(traj.n_frames, traj.n_atoms * 3))
 
-
-
-
-#plot the data reshaped above into the 2D space created before
+#plot the 2 component data reshaped above into the 2D space created before
 plt.figure()
 plt.scatter(reduced_cartesian[:, 0], reduced_cartesian[:, 1], marker='x', c=traj.time)
 plt.xlabel('PC1')
@@ -46,7 +38,7 @@ cbar = plt.colorbar()
 cbar.set_label('legend')
 plt.show()
 
-#variance plot
+#plot the variance of the n-component system created above
 plt.figure()
 plt.plot(pca_all.explained_variance_ratio_)
 plt.title("PCA Analysis")
